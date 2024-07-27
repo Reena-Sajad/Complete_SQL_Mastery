d7a06c79bea80871e2108f3a3729ba8146722170
Reena-Sajad <79794606+Reena-Sajad@users.noreply.github.com>
2024-07-27 04:20:31 -0400

    Update Module 2.sql

--- Module 2.sql
+++ Module 2.sql
@@ -12,9 +12,30 @@ USE sql_store;
 
 SELECT *
 FROM orders
-WHERE '2018-01-01' < order_date 
+WHERE '2018-01-01' < order_date;
 
--- OPERATORS
+-- OPERATORS: NOT, AND, OR
 SELECT *
 FROM order_items
-WHERE order_id = 6 AND (unit_price*quantity) > 30
+WHERE order_id = 6 AND (unit_price*quantity) > 30;
+
+-- IN OPERATOR
+SELECT *
+FROM products
+WHERE quantity_in_stock IN (49,38,72);
+
+-- BETWEEN OPERATOR
+SELECT * 
+FROM customers
+WHERE birth_date BETWEEN '1990-01-01' AND '2000-01-01';
+
+-- LIKE OPERATOR
+-- % - Any number of characters
+-- _ - Specific number of characters 
+SELECT *
+FROM customers
+WHERE address LIKE '%trail%' OR address LIKE '%avenue%';
+
+SELECT *
+FROM customers
+WHERE phone LIKE '%9';
