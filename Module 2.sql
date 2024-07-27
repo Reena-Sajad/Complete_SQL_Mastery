4cfe9ca1ea4100eb6ccf863c0258e87970950cca
Reena-Sajad <79794606+Reena-Sajad@users.noreply.github.com>
2024-07-27 03:44:50 -0400

    Completed Exercises 2.2 and 2.3

--- Module 2.sql
+++ Module 2.sql
@@ -1,6 +1,20 @@
 USE sql_store;
+
+-- SELECT CLAUSE
 SELECT 
 name,
 unit_price,
 unit_price * 1.1 AS new_price
 FROM products;
+
+-- WHERE CLAUSE
+USE sql_store;
+
+SELECT *
+FROM orders
+WHERE '2018-01-01' < order_date 
+
+-- OPERATORS
+SELECT *
+FROM order_items
+WHERE order_id = 6 AND (unit_price*quantity) > 30
