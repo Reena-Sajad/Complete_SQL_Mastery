d4a065822a1da41cb2a307b4c28c5fec56f2ee57
Reena-Sajad <79794606+Reena-Sajad@users.noreply.github.com>
2024-07-29 09:13:39 -0400

    Completed Ex 3.1

--- Module 3.sql
+++ Module 3.sql
@@ -1,1 +1,7 @@
--- INNER JOINS
+-- INNER JOINS The INNER JOIN keyword selects records that have matching values in both tables. JOIN generally refers to inner join
+use sql_store;
+
+SELECT order_id, oi.product_id, quantity, oi.unit_price
+from order_items oi
+JOIN products p 
+ON oi.product_id = p.product_id
