4acfa6bef6fabd72984c350829e2e84b1c44e7f4
Reena-Sajad <79794606+Reena-Sajad@users.noreply.github.com>
2024-07-31 07:14:04 -0400

    Add files via upload

---
+++ Module 4.sql
@@ -0,0 +1,21 @@
+-- 4.1 COLUMN ATTRIBUTES 
+-- PK: primary key, NN: Not Null, AI: Auto Increment etc
+
+-- 4.2 Inserting a Row
+-- INSERT INTO statement, VALUES() keyword
+
+INSERT INTO sql_store.customers 
+VALUES(
+DEFAULT,
+'Reena',
+'Sajad',
+NULL,
+NULL,
+'address',
+'city',
+'ME',
+DEFAULT);
+
+-- 4.3 Inserting multiple rows
+
+
