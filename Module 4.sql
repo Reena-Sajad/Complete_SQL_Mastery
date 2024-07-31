bbca22b97bce8882d382594de712158be80f98ef
Reena-Sajad <79794606+Reena-Sajad@users.noreply.github.com>
2024-07-31 08:28:56 -0400

    Update Module 4.sql

--- Module 4.sql
+++ Module 4.sql
@@ -17,5 +17,14 @@ NULL,
 DEFAULT);
 
 -- 4.3 Inserting multiple rows
+-- Use multiple parenthesis after the values keyword
+
+INSERT INTO sql_store.products (name, quantity_in_stock, unit_price)
+VALUES 
+('beans', 45, 1.45),
+('potatoes', 56, 2.67),
+('boots', 23, 5.70);
+
+-- 4.4 Inserting Hierarchical Rows
 
 
